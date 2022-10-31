1db38032282d2744ec1ae455374a1b7b922fd6a9
Jonny <nates.jonnygs@gmail.com>
2022-10-31 20:19:53 +0200

    Fixed DB client connection error

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ for (const folder of functionFolders) {
   const functionFiles = fs
     .readdirSync(`./src/functions/${folder}`)
     .filter((file) => file.endsWith(".js"));
-  for (const file of functionFiles) require(`./functions/${folder}/${file}`)(client);
+  for (const file of functionFiles)
+    require(`./functions/${folder}/${file}`)(client);
 }
 
 client.handleEvents();
