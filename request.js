f757f13556dc49b86f1716174b4deddec09e666e
jonnynates <nates.jonnygs@gmail.com>
2022-11-29 11:22:57 +0000

    initial commit

--- request.js
+++
@@ -1,8 +0,0 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
-
-// module.exports = {
-//   name: "request",
-//   description: "looking for game command",
-//   async execute(client, message, args) {},
-// };
