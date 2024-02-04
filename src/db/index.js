20ad8bb085473310783d68c48a1efb9e88065268
Jonny <nates.jonnygs@gmail.com>
2024-02-05 01:42:41 +0200

    Removed connection string from DB setup

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool();
 
 module.exports = {
   async query(text, params) {
