7a72ef38df3346e4367e5bcab5960ac1f85d7a07
Jonny <nates.jonnygs@gmail.com>
2024-02-06 22:34:27 +0200

    trigger build

--- src/db/index.js
+++ src/db/index.js
@@ -9,7 +9,7 @@ module.exports = {
     const start = Date.now();
     const res = await pool.query(text, params);
     const duration = Date.now() - start;
-    console.log("executed query", { text, duration, rows: res.rowCount });
+    console.log("executed query:", { text, duration, rows: res.rowCount });
     return res;
   },
   async getClient() {
