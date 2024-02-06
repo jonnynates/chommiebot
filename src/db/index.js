e47bf7853ba37c03246405631b843dfce7779014
Jonny <nates.jonnygs@gmail.com>
2024-02-06 22:27:55 +0200

    connection string approach

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 module.exports = {
   async query(text, params) {
